ef037a04c36e6d9d29adedd28a158692d6f3805c
Olivier Lacan <olivier.lacan@gmail.com>
2012-03-17 21:26:31 -0400

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flock::Application.config.secret_token = 'da7517f6a7e419fd3799930d36738e093acba3b001cc78f60746c22084c2d55643e2fe07bdce1e690f2d0bbee66b50c8311efd9986d3a6df8e3da1d6fb6cb577'
