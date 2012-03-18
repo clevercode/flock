6be332ec57c128801d12e1404055d5c015b78ee7
Andrew Smith <fullsailor@me.com>
2012-03-18 03:26:28 -0400

    Add a Team model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120318025547) do
+ActiveRecord::Schema.define(:version => 20120318072205) do
+
+  create_table "teams", :force => true do |t|
+    t.string "name"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "first_name",             :default => "", :null => false
