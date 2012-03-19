417943a26c0a6508fe2ebe11edafdfa7f3926d9d
Olivier Lacan <olivier.lacan@gmail.com>
2012-03-19 02:59:39 -0400

    Added new/create (and form partial) to Teams.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,27 @@
 class TeamsController < ApplicationController
+  respond_to :html
+
+  def index
+    @team = current_user.teams
+  end
 
   def show
     @team = Team.find(params[:id])
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.create(params[:team])
+    if @team.valid?
+      flash[:notice] = "Hey look at that, a new team!"
+      respond_with @team
+    else
+      flash[:notice] = "Couldn't create that team, looks like it's forever alone for you..."
+      render :new
+    end
   end
 
 end
