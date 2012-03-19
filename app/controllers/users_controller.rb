e476f44a59f96e2eb8b65f25cf24878c92354a2a
Olivier Lacan <olivier.lacan@gmail.com>
2012-03-19 02:37:34 -0400

    Generated users controller & views (CRUD).

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  respond_to :html
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    if @user = User.create(params[:user])
+      flash[:notice] = "Nice job, you've created your flock account!"
+      sign_in(:user, @user)
+    else
+      flash[:notice] = "We couldn't create your flock account, sorry about that."
+    end
+    redirect_to root_path
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    respond_with @user.update_attributes(params[:user])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    respond_with @user.destroy
+  end
+end
